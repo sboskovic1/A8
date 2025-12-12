5ded71964b26ee61eb1d0d2fb1120cca1477a97d
TJ Li <TianjianLi314@gmail.com>
2025-12-12 00:39:39 -0600

    scripts and gitignore

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+cd /storage-home/t/tl107/comp432/A8/Build
+echo 11 | ~/scons/bin/scons-3.1.2
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+cd /storage-home/s/sb121/comp530/A7/Build/bin
+echo 11 | ~/scons/bin/scons-3.1.2
+./sqlUnitTest 
